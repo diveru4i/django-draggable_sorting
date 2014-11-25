e33e050b3801be2de5fd0f96e39fcaeaddf9092c
Rabbit Silver <diveru4i@gmail.com>
2014-11-25 19:27:23 +0700

    002

--- draggable_sorting/__init__.py
+++ draggable_sorting/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 class DraggableMixin(object):
-    class Media:
-        js = (
+    def __init__(self, *args, **kwargs):
+        self.Media.js = list(self.Media.js) + [
             '/static/draggable_sorting/vendor/jquery-ui/jquery-ui.js',
             '/static/draggable_sorting/js/draggable.js',
-            '/static/draggable_sorting/js/deleteAll.js',
-        )
+        ]
+        super(Dr, self).__init__(*args, **kwargs)
